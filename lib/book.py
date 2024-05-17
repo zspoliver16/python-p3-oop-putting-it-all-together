aa45b4d673dc6ea39312ed196e3dff9178a927cb
zachary oliver <zspoliver@gmail.com>
2024-05-17 14:51:50 -0400

    adds p3 oop lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None
+        self.set_page_count(page_count)
+        
+        
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+            
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+        
+    page_count = property(get_page_count, set_page_count)
+    
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+        self.condition = None  
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
